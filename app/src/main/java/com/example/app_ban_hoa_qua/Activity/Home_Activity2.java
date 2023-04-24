1531fa20bdda7b2718c5b286f3bb26f44cf82567
dongthach <dong21042002@gmail.com>
2023-04-24 17:08:31 +0700

    update giao dien

---
+++ app/src/main/java/com/example/app_ban_hoa_qua/Activity/Home_Activity2.java
@@ -0,0 +1,32 @@
+package com.example.app_ban_hoa_qua.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.app_ban_hoa_qua.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home_Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_2);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard,R.id.add_foodFragment, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setTitle("LOL");
+    }
+
+}
